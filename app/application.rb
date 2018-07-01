991f026ae1d1c476e36277e949349f4a68179866
Gabriel Valle <gvalle91@gmail.com>
2018-07-01 15:44:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,13 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
+      item_search = req.path.split("/items/")
+      if @@items.include?(item_search)
 
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
     else
       resp.status = 404
     end
